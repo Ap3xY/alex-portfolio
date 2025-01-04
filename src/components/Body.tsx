beb4d9770ff7f074a7fb83977bffd2996e1c4380
Alex <alexvdberg010109@gmail.com>
2025-01-04 13:50:35 +0200

    feat(tech stack): added the tech stack section

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,6 +1,22 @@
 export default function Body() {
   return (
-    <div>Body</div>
+    <div className="flex flex-col mt-20 gap-20 p-2 text-center mx-auto">
+      <section>
+        <h2 className="text-2xl mb-10 w-1/2 mx-auto font-bold bg-red-400 p-2 rounded-lg">Tech Stack</h2>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2">
+              <div className="bg-red-400 p-2 rounded-lg">React</div>
+              <div className="bg-red-400 p-2 rounded-lg">Next.js</div>
+              <div className="bg-red-400 p-2 rounded-lg">Tailwind CSS</div>
+              <div className="bg-red-400 p-2 rounded-lg">TypeScript</div>
+              <div className="bg-red-400 p-2 rounded-lg">JavaScript</div>
+            </div>
+            <div className="flex gap-2"></div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 
 export default function Header() {
   return (
-    <header>
-      <nav>
-        <ul className="flex gap-2">
-          <li>Contact</li>
-          <li>Tech Stack</li>
-          <li>Education</li>
-        </ul>
-      </nav>
+    <header className="flex flex-col items-center p-4 gap-4">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Hi, I'm <span className="text-red-400">Alex</span>. <br /> Welcome to my portfolio!</h1>
+        </div>
+      </div>
+      <div>
+        <nav className="flex gap-2">
+          <a className="bg-red-400 p-2 rounded-lg">Contact</a>
+          <a className="bg-red-400 p-2 rounded-lg">Tech Stack</a>
+          <a className="bg-red-400 p-2 rounded-lg">Education</a>
+        </nav>
+      </div>
     </header>
   )
 }
